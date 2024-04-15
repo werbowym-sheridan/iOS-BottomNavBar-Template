f123eec0be6bec13af83908bd7c4467123e6fea0
werbowym-sheridan <werbowym@sheridancollege.ca>
2024-04-15 19:35:06 -0400

    Initial Commit

---
+++ BottomNavBar-Template/BottomNavBar_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomNavBar_TemplateApp.swift
+//  BottomNavBar-Template
+//
+//  Created by Michael Werbowy on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct BottomNavBar_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
